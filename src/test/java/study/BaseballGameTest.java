3a56e78a945c187b0ee2b02267ea6babc23292cd
Leeho <ho.sol.lee@gmail.com>
2021-09-12 22:13:23 +0900

    Revert "refactor: 정답 set부분 리팩토링"
    This reverts commit eced16940af4c90f3847f215731a904ab41a62cb.

--- src/test/java/study/BaseballGameTest.java
+++ src/test/java/study/BaseballGameTest.java
@@ -1,17 +1,17 @@
 package study;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+
+// 1. write Failing Test
+// 2. write production code that could pass test
+// 3. refactoring
 
 class BaseballGameTest {
 
@@ -24,15 +24,13 @@ class BaseballGameTest {
 
 	@Test
 	void answer_is_string_that_have_length_of_3_after_setAnswer() {
-		assertEquals(baseballGame.getAnswer().size(), 3);
+		assertEquals(baseballGame.answer.size(), 3);
 	}
 
 	@Test
 	void answer_is_diffrent_each_other() {
-		assertEquals(baseballGame.getAnswer().size(),
-			baseballGame.getAnswer().stream().distinct().collect(Collectors.toList()).size());
-	}
-
+		assertEquals(baseballGame.answer.size(),
+			baseballGame.answer.stream().distinct().collect(Collectors.toList()).size());
 	@ParameterizedTest
 	@ValueSource(strings = {"24", "1ㅁ2", "7a26", "asd", "   "})
 	void validate_users_input_false(String input) {
@@ -42,25 +40,4 @@ class BaseballGameTest {
 			.hasMessage("입력값이 올바르지 않습니다");
 	}
 
-	@ParameterizedTest
-	@CsvSource(value = {"246:0,0", "245:0,1", "285:0,2", "518:1,2", "185:1,2", "157,1:1", "158:3,0"}, delimiter = ':')
-	void checkStrike_and_checkBall(String input, String strikeAndBall) {
-
-		// GIVEN
-		String[] result = strikeAndBall.split(",");
-
-		List<Integer> answer = new ArrayList<Integer>();
-		answer.add(1);
-		answer.add(5);
-		answer.add(8);
-		baseballGame.setAnswer(answer);
-
-		// WHEN
-		baseballGame.count(input);
-
-		// THEN
-		assertThat(baseballGame.getStrike()).isEqualTo(Integer.parseInt(result[0]));
-		assertThat(baseballGame.getBall()).isEqualTo(Integer.parseInt(result[1]));
-
-	}
 }
