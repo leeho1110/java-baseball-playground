cfa09822f377809f627a90d35dc9c3f5dbc6e457
Leeho <ho.sol.lee@gmail.com>
2021-09-12 22:16:08 +0900

    feat: gameStrat() 메소드
    전체 비즈니스 로직을 수행하는 메소드

--- src/main/java/study/BaseballGame.java
+++ src/main/java/study/BaseballGame.java
@@ -10,6 +10,7 @@ public class BaseballGame {
 	private int strike;
 	private int ball;
 
+	private boolean gameSet;
 
 	public BaseballGame() {
 		this.answer = new ArrayList<Integer>();
@@ -27,11 +28,64 @@ public class BaseballGame {
 			this.getAnswer().add(ball);
 		}
 	}
+
+	public void gameStart() {
+		InputView inputView = new InputView();
+		ResultView resultView = new ResultView();
+
+		while (!gameSet) {
+			String input = inputView.getInput();
+
+			// validate
+			validate(input);
+
+			// count
+			count(input);
+
+			// result
+			System.out.println(resultView.printResult(this.strike, this.ball));
+		}
+	}
+
+	public void validate(String input) {
+		if (!input.matches("^[0-9]{3}")) {
+			throw new IllegalArgumentException("입력값이 올바르지 않습니다");
 		}
 	}
 
 	public void count(String input) {
-		// TODO Auto-generated method stub
+		for (int idx = 0; idx < answer.size(); idx++) {
+			int inputInt = Integer.parseInt(String.valueOf(input.charAt(idx)));
+			checkStrike(inputInt, idx);
+			checkBall(inputInt);
+		}
+	}
 
+	public void checkStrike(int input, int idx) {
+		if (input == answer.get(idx)) {
+			this.strike = getStrike() + 1;
+		}
+	}
+
+	public void checkBall(int input) {
+		if (answer.contains(input)) {
+			this.ball = getBall() + 1;
+		}
+	}
+
+	public void setAnswer(List<Integer> answer) {
+		this.answer = answer;
+	}
+
+	public List<Integer> getAnswer() {
+		return this.answer;
+	}
+
+	public int getStrike() {
+		return strike;
+	}
+
+	public int getBall() {
+		return ball;
 	}
 }
