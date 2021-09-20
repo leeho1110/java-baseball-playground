a2799d61ba088353b04b5f779b443a2c15479b13
Ho-Lee <ho.sol.lee@gmail.com>
2021-09-20 16:39:15 +0900

    chore: package 변경

--- src/test/java/study/calculator/CalculatorBasicTest.java
+++ src/test/java/study/calculator/CalculatorBasicTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import study.calculator.CalculatorBasic;
 
 class CalculatorBasicTest {
 
--- src/test/java/study/calculator/CalculatorSecondTest.java
+++ src/test/java/study/calculator/CalculatorSecondTest.java
@@ -1,4 +1,4 @@
-package study;
+package study.calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import study.calculator.Calculator;
 
 class CalculatorSecondTest {
 
