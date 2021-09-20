374df47e2553f1b841e9a49517503a2756275796
Ho-Lee <ho.sol.lee@gmail.com>
2021-09-20 22:53:33 +0900

    test: 테스트 코드 분리

--- src/test/java/study/baseball/BallsTest.java
+++ src/test/java/study/baseball/BallsTest.java
@@ -1,35 +1,13 @@
 package study.baseball;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import java.util.Arrays;
 
-public class BaseballGameTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-	@Test
-	@DisplayName("Ball 객체 하나와 하나  비교 시 strike")
-	void check_1_1_strike() {
-		Ball computer = new Ball(1, 1);
-		assertThat(computer.judgeBall(new Ball(1, 1))).isEqualTo(BallStatus.STRIKE);
-	}
-
-	@Test
-	@DisplayName("Ball 객체 하나와 하나 비교 시 ball")
-	void check_1_1_ball() {
-		Ball computer = new Ball(1, 1);
-		assertThat(computer.judgeBall(new Ball(3, 1))).isEqualTo(BallStatus.BALL);
-	}
-
-	@Test
-	@DisplayName("Ball 객체 하나와 하나 비교 시 nothing")
-	void check_1_1_nothing() {
-		Ball computer = new Ball(1, 1);
-		assertThat(computer.judgeBall(new Ball(2, 3))).isEqualTo(BallStatus.NOTHING);
-	}
-
+public class BallsTest {
 	@Test
 	@DisplayName("Ball 객체 3개가 담겨있는 리스트를 돌며 하나와 하나 비교 시 nothing")
 	void check_3_1_nothing() {
