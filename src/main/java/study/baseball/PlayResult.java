2550589f2204ddd2c93af69be9bebc7389a1aeb5
Ho-Lee <ho.sol.lee@gmail.com>
2021-09-21 22:12:00 +0900

    feat: 결과값 출력 및 3 스트라이크 시 게임을 중단하는 메소드 추가

--- src/main/java/study/baseball/PlayResult.java
+++ src/main/java/study/baseball/PlayResult.java
@@ -3,6 +3,7 @@ package study.baseball;
 public class PlayResult {
 	private int ball;
 	private int strike;
+	private boolean gameSet = true;
 
 	public int getBall(){
 		return this.ball;
@@ -19,5 +20,20 @@ public class PlayResult {
 		if(status.isBall()){
 			this.ball += 1;
 		}
+	}
+
+	public void checkGameProgress() {
+		if(getStrike() == 3){
+			gameSet = false;
+			System.out.println(">>> 정답입니다! ");
+		}
+	}
+
+	public boolean isGameSet() {
+		return gameSet;
+	}
+
+	public void printResult() {
+		System.out.printf("[%d] strike [%d] ball ! \n\n",getStrike(),getBall());
 	}
 }
